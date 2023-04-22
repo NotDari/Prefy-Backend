8c316bf22fbf67a04cbc0ab823b33bf290629fdb
dari <dariushtomlinson@gmail.com>
2023-04-22 19:42:14 +0100

    Docker allowing http

--- src/main/java/com/daribear/PrefyBackend/PrefyBackendApplication.java
+++ src/main/java/com/daribear/PrefyBackend/PrefyBackendApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
